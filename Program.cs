84982f906e4e3269bec9684cd66d55dc51b6593c
Nathan Carvalho <67009187+CarvalhoNathan@users.noreply.github.com>
2025-06-09 10:07:54 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,153 +6,160 @@ bool ferido = false, inimigoproximo = false, eliminado = false;
 bool rodando = true; 
 int roll, ciclo = 0;
 
+Console.WriteLine("--- Simulação de IA de NPC ---");
+Console.WriteLine(" ");
+
 Random dado = new Random();
 
 while (rodando == true){
-while (morto == false)
-{
-    while (procurando == true)
+    while (morto == false)
     {
-        roll = dado.Next(1, 3);
+        // ---------------------- Procurando ----------------------
+        while (procurando == true)
+        {
+            roll = dado.Next(1, 3);
 
-        if (roll == 1)
-        {
-            ferido = false;
-        }
-        else
-        {
-            inimigoproximo = true;
-        }
-
-        if (eliminado == false && (ferido == true || inimigoproximo == false))
-        {
-            procurando = true;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Procurando > Procurando");
-        }
-        else if (eliminado == false && ferido == false && inimigoproximo == true)
-        {
-            atacando = true;
-            procurando = false;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Procurando > Atacando");
-        }
-    }
-
-
-    while (atacando == true)
-    {
-        roll = dado.Next(1, 3);
-        if (roll == 1)
-        {
-            ferido = true;
-            roll = dado.Next(1, 2);
-            if (roll == 2)
+            if (roll == 1)
             {
-                eliminado = true;
+                ferido = false; // 50% de chances de curar-se.
+            }
+            else
+            {
+                inimigoproximo = true; // 50% de chances de encontrar o inimigo.
+            }
+
+            if (eliminado == false && (ferido == true || inimigoproximo == false))
+            {
+                procurando = true;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Procurando => Procurando (InimigoProximo = N, Ferido = S)");
+            }
+            else if (eliminado == false && ferido == false && inimigoproximo == true)
+            {
+                atacando = true;
+                procurando = false;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Procurando => Atacando (InimigoProximo = S, Eliminado = N, Ferido = N)");
             }
         }
-        else
+
+        // ---------------------- Atacando ----------------------
+        while (atacando == true)
         {
-            inimigoproximo = false;
+            roll = dado.Next(1, 3);
+            if (roll == 1)
+            {
+                ferido = true; // 50% de chances de se ferir.
+
+                roll = dado.Next(1, 2);
+                if (roll == 2)
+                {
+                    eliminado = true; // 50% de chances de morrer com o ferimento.
+                }
+            }
+            else
+            {
+                inimigoproximo = false; // 50% de chances de matar o inimigo ou o inimigo fugir.
+            }
+
+            if (eliminado == false && ferido == false && inimigoproximo == true)
+            {
+                atacando = true;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Atacando => Atacando (InimigoProximo = S, Eliminado = N, Ferido = N)");
+            }
+            else if (eliminado == false && inimigoproximo == false)
+            {
+                procurando = true;
+                atacando = false;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Atacando => Procurando (InimigoProximo = S, Eliminado = N, Ferido = N)");
+            }
+            else if (eliminado == false && ferido == true)
+            {
+                fugindo = true;
+                atacando = false;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Atacando => Fugindo (Eliminado = N, Ferido = S)");
+            }
+            else if (eliminado == true)
+            {
+                atacando = false;
+                morto = true;
+                Console.WriteLine($"# {ciclo + 1} — Atacando => Morto (Eliminado = S, Ferido = S)");
+            }
         }
 
+        // ---------------------- Fugindo ----------------------
+        while (fugindo == true)
+        {
+            roll = dado.Next(1, 5);
+            if (roll == 1)
+            {
+                eliminado = true; // 25% de chances de morrer com o ferimento.
+            }
+            else if (roll == 2)
+            {
+                ferido = false; // 25% de chances de curar-se.
+            }
+            else
+            {
+                inimigoproximo = false; // 50% de chances do inimigo desistir.
+            }
 
-        if (eliminado == false && ferido == false && inimigoproximo == true)
-        {
-            atacando = true;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Atacando > Atacando");
-        }
-        else if (eliminado == false && inimigoproximo == false)
-        {
-            procurando = true;
-            atacando = false;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Atacando > Procurando");
-        }
-        else if (eliminado == false && ferido == true)
-        {
-            fugindo = true;
-            atacando = false;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Atacando > Fugindo");
-        }
-        else if (eliminado == true)
-        {
-            atacando = false;
-            morto = true;
+            if (eliminado == false && ferido == true)
+            {
+                fugindo = true;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Fugindo => Fugindo (Eliminado = N, Ferido = S)");
+            }
+            else if (eliminado == false && ferido == false)
+            {
+                procurando = true;
+                fugindo = false;
+                ciclo = ciclo + 1;
+                Console.WriteLine($"# {ciclo} — Fugindo => Procurando (Eliminado = N, Ferido = N)");
+            }
+            else if (eliminado == true)
+            {
+                morto = true;
+                fugindo = false;
+                Console.WriteLine($"# {ciclo + 1} — Fugindo => Morto (Eliminado = S, Ferido = S)");
+            }
         }
     }
 
-    while (fugindo == true)
+    Console.WriteLine(" ");
+    Console.WriteLine($"O NPC sobreviveu por {ciclo} transições.");
+    Console.WriteLine(" ");
+
+    Console.WriteLine("Deseja ir novamente? (S/N)");
+    string resposta = Console.ReadLine()!.Remove(1).ToUpper();
+
+    while (!(resposta == "S") && !(resposta == "N"))
     {
-        roll = dado.Next(1, 5);
-        if (roll == 1)
-        {
-            eliminado = true;
-        }
-        else if (roll == 2)
-        {
-            ferido = false;
-        }
-        else
-        {
-            inimigoproximo = false;
-        }
-
-        if (eliminado == false && ferido == true)
-        {
-            fugindo = true;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Fugindo > Fugindo");
-        }
-        else if (eliminado == false && ferido == false)
-        {
-            procurando = true;
-            fugindo = false;
-            ciclo = ciclo + 1;
-            Console.WriteLine("Fugindo > Procurando");
-        }
-        else if (eliminado == true)
-        {
-            morto = true;
-            fugindo = false;
-        }
-
+        Console.WriteLine("\nOpção inválida! Tente novamente.");
+        resposta = Console.ReadLine()!.Remove(1).ToUpper();
     }
 
-}
+    switch (resposta)
+    {
+        case "S":
+            resposta = "null";
+            morto = false;
+            procurando = true;
+            atacando = false;
+            fugindo = false;
+            ferido = false;
+            inimigoproximo = false;
+            eliminado = false;
+            ciclo = 0;
+            rodando = true;
+            break;
 
-Console.WriteLine($"O NPC sobreviveu por {ciclo} transições.");
-
-
-Console.WriteLine("Deseja ir novamente? (S/N)");
-string resposta = Console.ReadLine()!.Remove(1).ToUpper();
-
-while (!(resposta == "S") && !(resposta == "N"))
-{
-    Console.WriteLine("\nOpção inválida! Tente novamente.");
-    resposta = Console.ReadLine()!.Remove(1).ToUpper();
-}
-
-switch(resposta){
-    case "S":
-    resposta = "null";
-    morto = false;
-    procurando = true;
-    atacando = false;
-    fugindo = false;
-    ferido = false;
-    inimigoproximo = false;
-    eliminado = false;
-    ciclo = 0;
-    rodando = true;
-    break;
-
-    case "N":
-    resposta = "null";
-    rodando = false;
-    break;
-}
+        case "N":
+            resposta = "null";
+            rodando = false;
+            break;
+    }
 }
